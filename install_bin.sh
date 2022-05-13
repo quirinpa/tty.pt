32d8e255d29cd7cfbd8c166a9d7b62a36f69a9f0
quirinpa <quirinpa@tty.pt>
2022-05-13 23:42:13 +0200

    Added install_bin.sh

---
+++ install_bin.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+path="`which $1`"
+ROOT=/var/www
+ldd $path | awk '{ print $7 }' | tail -n +3 | while read line; do
+	target_path="`dirname $line`"
+	[[ -d "$ROOT$target_path" ]] || mkdir -p $target_path
+	[[ -f "$ROOT$line" ]] || cp $line $ROOT$line
+done
+echo $path >> .instal_lbin
