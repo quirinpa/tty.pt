e16e327ecddfab58604c562fb636388c7ee7dcdf
quirinpa <quirinpa@tty.pt>
2023-04-17 08:01:09 +0100

    bang

---
+++ resize-all.sh
@@ -0,0 +1,19 @@
+#!/bin/ksh
+ROOT=/var/www
+
+find $ROOT/htdocs/img -type f | while read line; do
+	dname="`dirname $line`"
+	bname="`basename $line`"
+	cd $dname
+	convert -resize x128 $bname small-$bname
+	cd -
+done
+
+# find $ROOT/shops/loja_dos_sonhos/ -type f -name "images" | while read line; do
+# 	echo $line:
+# 	cat $line
+# 	cat $line | while read line2; do
+# 		echo `basename $line2` `dirname $line2`
+# 	done
+
+# done
