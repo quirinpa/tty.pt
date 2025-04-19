643aa3020a8c2ddab6dc36ecd055892d929e26ff
quirinpa <quirinpa@tty.pt>
2025-04-19 16:52:57 +0100

    SubIndex/Index path updates, easy buttons, valid fails

--- index_put.sh
+++ index_put.sh
@@ -2,6 +2,7 @@
 target=index.db
 test $# -lt 1 || target=$1
 qhash="`pnpm root -w`/@tty-pt/qhash/qhash"
+#qhash=qhash
 cmd="$qhash `while read line; do echo -n " -p '$line'"; done` $target"
 echo "$cmd"
 sh -c "$cmd"
