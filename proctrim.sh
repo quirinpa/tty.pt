aaf7dbfd990f97fffd57d74224662f1e54565913
quirinpa <quirinpa@tty.pt>
2025-05-17 00:52:46 +0100

    No spam with proctrim, better custom index, delete unused, cached qdb

--- proctrim.sh
+++ proctrim.sh
@@ -7,4 +7,4 @@ PID="`pgrep $PROCESS_NAME`"
 test -n "$PID" || continue
 CPU_USAGE="`ps -p $PID -o %cpu | tail -n 1 | awk '{print int($1)}'`"
 test "$CPU_USAGE" -le "$THRESHOLD" || exit 0
-rcctl restart nd
+rcctl restart nd >/dev/null 2>&1
